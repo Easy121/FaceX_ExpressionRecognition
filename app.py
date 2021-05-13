0377ab916da76e78981cc98d78e660f6f5a614ad
Zijun Guo <18811434432@163.com>
2021-05-14 01:16:59 +0800

    Visualization Updated - able to output .gif document - simpler codes with class defined

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 from utility.utility import *
+from utility.visualizer import *
 
 
 # 导入numpy压缩数据，列表类型
 data = np.load('data/Abstract_NPZ/Female_front/happy.npz', allow_pickle=True)
-stroke_visualizer(data[500])
+# 可视化
+vis = Visualizer(data[500])
+# vis.visualizer_stroke()
+vis.visualizer_stroke_gif()
 
+# # x = [[1, 2, 3], [1, 2, 3]]
+# # y = [[1, 2, 3], [3, 2, 1]]
+# x = np.array([[1, 3], [1, 3]])
+# y = np.array([[1, 3], [3, 1]])
+# plt.plot(x, y)
+# plt.show()
 
+# # First set up the figure, the axis, and the plot element we want to animate
+# fig = plt.figure()
+# ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+# line, = ax.plot([], [], lw=2)
+#
+# # initialization function: plot the background of each frame
+# def init():
+#     line.set_data([], [])
+#     return line,
+#
+# # animation function.  This is called sequentially
+# def animate(i):
+#     x = np.linspace(0, 2, 1000)
+#     y = np.sin(2 * np.pi * (x - 0.01 * i))
+#     line.set_data(x, y)
+#     return line,
+#
+# # call the animator.  blit=True means only re-draw the parts that have changed.
+# anim = FuncAnimation(fig, animate, init_func=init,
+#                                frames=200, interval=20, blit=True)
+#
+# # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# # installed.  The extra_args ensure that the x264 codec is used, so that
+# # the video can be embedded in html5.  You may need to adjust this for
+# # your system: for more information, see
+# # http://matplotlib.sourceforge.net/api/animation_api.html
+# anim.save('basic_animation.gif', fps=30)
+#
+# plt.show()
--- utility/utility.py
+++ utility/utility.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def get_plot_range(data):
     """通过数据，得到方形xy轴范围"""
+    data = np.array(data)
     # 第一列为x，第二列为y
     x_max = np.max(data[:, 0])
     x_min = np.min(data[:, 0])
@@ -25,44 +25,6 @@ def get_plot_range(data):
         x_min = x_mid - plot_range / 2
 
     return np.array([[x_min, x_max], [y_min, y_max]])
-
-
-def stroke_visualizer(data):
-    """可视化笔触"""
-    # 数据处理
-    stroke_data_raw = np.array(data)
-    stroke_data = np.transpose(np.vstack((stroke_data_raw[:, 0], -stroke_data_raw[:, 1])))  # 上下颠倒
-    stroke_data = np.array(np.cumsum(stroke_data, axis=0, dtype=float))  # 按行累加
-    stroke_lift = np.array(stroke_data_raw[:, 2])
-
-    plt.figure(figsize=(3, 3), dpi=80)
-    plt.ion()  # 打开交互模式
-    # 设置绘图范围
-    plot_range = get_plot_range(stroke_data)
-    plt.xlim(plot_range[0, :])
-    plt.ylim(plot_range[1, :])
-    # 关闭坐标轴显示
-    plt.xticks([])
-    plt.yticks([])
-
-    # 向量前两个值为移动方向，最后一个值为是否提笔
-    point_former = stroke_data[0]  # 第一个点的位置
-    for i in range(1, len(stroke_data_raw)):
-        if stroke_lift[i-1] == 0:
-            # 此时要画
-            point_current = stroke_data[i]  # 当前点的位置
-            points = np.vstack((point_former, point_current))
-            plt.plot(points[:, 0], points[:, 1], linewidth='1', color='black', linestyle='-', marker='.')
-            point_former = point_current  # 下一个点的位置
-
-            plt.pause(0.01)  # 暂停
-        else:
-            # 此时不画
-            point_current = stroke_data[i]
-            point_former = point_current
-
-    plt.ioff()  # 关闭交互模式
-    plt.show()
 
 
 class Demo:
