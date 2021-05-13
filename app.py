436fee697ca9c54cd2510ef79ff88cd862ef4781
Zijun Guo <18811434432@163.com>
2021-05-13 17:44:15 +0800

    Ammendment to app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from utility.utility import *
 
 
 # 导入numpy压缩数据，列表类型
-data = np.load('data/happy.npz', allow_pickle=True)
+data = np.load('data/Abstract_NPZ/Female_front/happy.npz', allow_pickle=True)
 stroke_visualizer(data[500])
 
 
