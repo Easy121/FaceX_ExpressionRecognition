1073e4852ad3d8578f931b782391c7527e9517e0
Zijun Guo <18811434432@163.com>
2021-05-14 09:33:23 +0800

    Visual Upgrade - use enumerate to reduce codes - simplified animation with higher efficiency

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ from utility.visualizer import *
 data = np.load('data/Abstract_NPZ/Female_front/happy.npz', allow_pickle=True)
 # 可视化
 vis = Visualizer(data[500])
-vis.visualizer_stroke()
-# vis.visualizer_stroke_gif()
+# vis.visualizer_stroke()
+vis.visualizer_stroke_gif()
--- utility/visualizer.py
+++ utility/visualizer.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from utility.utility import *
 import matplotlib.pyplot as plt
-from matplotlib.animation import ArtistAnimation, FuncAnimation
+from matplotlib.animation import ArtistAnimation, FuncAnimation, PillowWriter
 
 
 class Visualizer:
@@ -12,19 +12,15 @@ class Visualizer:
         stroke_data = np.transpose(np.vstack((self.stroke_data_raw[:, 0], -self.stroke_data_raw[:, 1])))  # 上下颠倒
         stroke_data = np.cumsum(stroke_data, axis=0, dtype=float).tolist()  # 按行累加
         stroke_lift = self.stroke_data_raw[:, 2].tolist()
-        stroke_data_sequential = [[]]
+        stroke_data_sequential = []
 
         # 向量前两个值为移动方向，最后一个值为是否提笔
-        j = 0
-        for i in range(len(stroke_lift)):
-            if stroke_lift[i] == 0:
-                stroke_data_sequential[j].append(stroke_data[i])
-            else:
-                stroke_data_sequential[j].append(stroke_data[i])
-                if i != len(stroke_lift) - 1:
-                    stroke_data_sequential.append([])
-                stroke_data_sequential[j] = np.transpose(np.array(stroke_data_sequential[j])).tolist()
-                j += 1
+        values = []
+        for count, value in enumerate(stroke_lift):
+            values.append(stroke_data[count])
+            if value == 1:
+                stroke_data_sequential.append(np.transpose(np.array(values)).tolist())
+                values = []
 
         self.stroke_data = stroke_data
         self.stroke_lift = stroke_lift
@@ -32,6 +28,7 @@ class Visualizer:
 
         self.fig, self.ax = plt.subplots(figsize=(3, 3), dpi=80)
         self.ln = self.ax.plot([], [], linewidth='1', color='black', linestyle='-', marker='.')
+        self.ims = []  # 每一帧需要plot的artistic集合
 
     def visualizer_stroke(self):
         """可视化笔触"""
@@ -43,20 +40,21 @@ class Visualizer:
         self.ax.xaxis.set_ticks([])
         self.ax.yaxis.set_ticks([])
 
-        j = 0
-        for data in self.stroke_data_sequential:
+        for count, data in enumerate(self.stroke_data_sequential):
             for k in range(len(data[0])):
-                for i in range(j):
-                    self.ax.plot(self.stroke_data_sequential[i][0], self.stroke_data_sequential[i][1],
+                im = []
+                for i in range(count):
+                    self.ax.plot(self.stroke_data_sequential[i][0],
+                                 self.stroke_data_sequential[i][1],
                                  linewidth='1', color='black', linestyle='-', marker='.')
-                self.ax.plot(data[0][0:(k+1)], data[1][0:(k+1)],
+                self.ax.plot(data[0][0:(k + 1)], data[1][0:(k + 1)],
                              linewidth='1', color='black', linestyle='-', marker='.')
                 plt.pause(0.01)  # 暂停
-            j += 1
 
         plt.show()
 
-    def init_stroke(self):
+    def visualizer_stroke_gif(self):
+        """可视化笔触，并生成gif动图"""
         # 设置绘图范围
         plot_range = get_plot_range(self.stroke_data)
         self.ax.set_xlim(plot_range[0, :])
@@ -64,32 +62,19 @@ class Visualizer:
         # 关闭坐标轴显示
         self.ax.xaxis.set_ticks([])
         self.ax.yaxis.set_ticks([])
-        return self.ln
 
-    def update_stroke(self, frame):
-        total_num = 0
-        stroke_num = 1
-        step_num = 0
-        for data in self.stroke_data_sequential:
-            total_num += len(data[0])
-            if total_num >= frame:
-                stroke_num -= 1
-                step_num = frame - (total_num - len(data[0]))
-                break
-            stroke_num += 1
+        for count, data in enumerate(self.stroke_data_sequential):
+            for k in range(len(data[0])):
+                im = []
+                for i in range(count):
+                    im.append(self.ax.plot(self.stroke_data_sequential[i][0],
+                                           self.stroke_data_sequential[i][1],
+                                           linewidth='1', color='black', linestyle='-', marker='.')[0])
+                im.append(self.ax.plot(data[0][0:(k + 1)], data[1][0:(k + 1)],
+                                       linewidth='1', color='black', linestyle='-', marker='.')[0])
+                self.ims.append(im)
+                plt.pause(0.01)  # 暂停
 
-        for i in range(stroke_num):
-            self.ln.append(self.ax.plot(self.stroke_data_sequential[i][0], self.stroke_data_sequential[i][1],
-                           linewidth='1', color='black', linestyle='-', marker='.'))
-        self.ln.append(self.ax.plot(self.stroke_data_sequential[stroke_num][0][0:(step_num+1)],
-                                    self.stroke_data_sequential[stroke_num][1][0:(step_num+1)],
-                                    linewidth='1', color='black', linestyle='-', marker='.'))
-
-        return self.ln
-
-    def visualizer_stroke_gif(self):
-        """可视化笔触，并生成gif动图"""
-        ani = FuncAnimation(self.fig, self.update_stroke, frames=len(self.stroke_data_raw),
-                            init_func=self.init_stroke, interval=50, blit=False)
-        # plt.show()
-        ani.save("figure/test.gif", writer='pillow', )
+        ani = ArtistAnimation(self.fig, self.ims, interval=40, repeat=True, blit=True)
+        ani.save(filename="figure/test.gif", writer='pillow')
+        plt.show()
